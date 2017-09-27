76c1d5d02413854a587158d0fe07be59667e1891
JackEWilson <jack.wilson.east@gmail.com>
2017-09-27 09:53:01 -0700

    added next button and new strings to strings.xml. not working yet

---
+++ app/src/main/java/com/bignerdranch/anroid/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.anroid.geoquiz;
+
+/**
+ * Created by battl on 9/27/2017.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+}
